7e6d3fbaba93e928c547664dbce28631920e7ab5
koyd9174 <koyd9174@naver.com>
2022-09-23 20:21:42 +0900

    shop

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { Card }  from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import bg from "./bg.jpg";
 
