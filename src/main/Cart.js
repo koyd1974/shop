e7ad6c1ad95e133bf62f52c8bb2612e918fba20b
koyd9174 <koyd9174@naver.com>
2022-09-23 20:21:09 +0900

    장바구니

--- src/main/Cart.js
+++ src/main/Cart.js
@@ -1,29 +1,62 @@
 import React from 'react';
 import {Table} from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux';
 
-function cart(){
+function Cart(props){
+  const state = useSelector((state)=>state);
+  const dispatch = useDispatch();
+  console.log(state)
+  
     return(
         <div>
-<Table>
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>상품명</th>
-      <th>수량</th>
-      <th>변경하기</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>안녕</td>
-      <td>안녕</td>
-      <td>안녕</td>
-    </tr>
-  </tbody>
+<Table responsive>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>상품명</th>
+            <th>가격</th>
+            <th>수량</th>
+            <th>변경</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            state.reducer1.map((obj, i) => {
+              return (
+                <tr key={i}>
+                  <td>{obj.id}</td>
+                  <td>{obj.name}</td>
+                  <td>{obj.price}</td>
+                  <td>{obj.qty}</td>
+                  <td>
+                    <a class="btn btn-secondary" role="button" onClick={() => {
+                      dispatch({
+                        type: 'plusQTY',
+                        data: obj.id
+                      });
+                    }}>+</a>
+                    &nbsp;
+                    <a class="btn btn-secondary" role="button" onClick={() => {
+                      dispatch({
+                        type: 'minusQTY',
+                        data: obj.id
+                      });
+                    }}>-</a>
+                  </td>
+                </tr>
+              )
+            })
+          }
+          {/* <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+          </tr> */}
+          </tbody>
 </Table>
-        </div>
+</div>
     )
 }
 
-export default cart;
+export default Cart;
