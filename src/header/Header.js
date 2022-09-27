02bbd0decef9934174dd17371d669b3e81854c9a
koyd9174 <koyd9174@naver.com>
2022-09-27 21:22:57 +0900

    login 기능 추가

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,8 +1,9 @@
-import React from 'react';
 import {NavLink} from 'react-router-dom';
 import { Navbar, Container} from 'react-bootstrap'
 
 function Header(props) {
+
+  
     return (
 		<>
     {/* Navbar */}
@@ -11,11 +12,14 @@ function Header(props) {
     <Navbar bg="light" variant="light">
         <Container>
             <h1><NavLink to ='/'style={{paddingLeft: 13, textDecoration: 'none'}}>PetShop</NavLink></h1>
+            
             <ul>
                     <li><NavLink to ='/upload/upload'style={{paddingLeft: 13, textDecoration: 'none'}}>상품등록</NavLink></li>
                     <li><NavLink to ='/product2/3'style={{paddingLeft: 13, textDecoration: 'none'}}>상품보기</NavLink></li>
                     <li><NavLink to ='/comunity/4'style={{paddingLeft: 13, textDecoration: 'none'}}>커뮤니티</NavLink></li>
                     <li><NavLink to ='/cart/5'style={{paddingLeft: 13, textDecoration: 'none'}}>장바구니</NavLink></li>
+                    <li><NavLink to ='/login/6'style={{paddingLeft: 13, textDecoration: 'none'}}>login</NavLink></li>
+                    <li><NavLink to ='/signup/7'style={{paddingLeft: 13, textDecoration: 'none'}}>signup</NavLink></li>
                 </ul>
         </Container>
       </Navbar>
