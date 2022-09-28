60c6f461c4719123f116841f5ac409cf666699be
koyd9174 <koyd9174@naver.com>
2022-09-28 21:25:17 +0900

    글쓰기기능 추가

--- src/main/Comunity.js
+++ src/main/Comunity.js
@@ -1,38 +1,51 @@
-import React, { useEffect } from 'react';
-import {Table} from 'react-bootstrap'   
-import { useNavigate } from 'react-router-dom';
-import { dbService } from '../fbase';
+import React, { Component, useEffect } from 'react';
+import { useState } from 'react';
+import { dbService } from '../fbase'
+import WriteButton from './WriteButton';
 
-const Comunity = (props) => {
-  const navigate = useNavigate()
-  
-  const onClick = ()=> {
-    navigate('/writing')
-  }
-    return(
-      <div>
-          <Table>
-            <thead>
-              <tr>
-                <th>순번</th>
-                <th> 제목 </th>
-                <th>글쓴이</th>
-                <th>날짜</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td></td>
-                <td>안녕</td>
-                <td>안녕</td>
-              </tr>
-            </tbody>
-          </Table>
-          <footer>
-            <button onClick={onClick}>글 쓰기</button>
-          </footer>
-      </div>
-    )
+const  Comunity = ({userObj})=> {
+    const [write, setWrite] = useState("")
+    const [writes, setWrites] = useState([])
+
+    useEffect(()=> {
+        dbService.collection('user').onSnapshot(snapshot => {
+            const writeArray = snapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data()
+            }))
+            setWrites(writeArray)
+        })
+    }, [])
+
+    const onSubmit = (e)=> {
+        e.preventDefault();
+    }
+    const onChange = (event)=> {
+        const {target : {value}} = event
+        setWrite(value)
+    }
+    const onClick = async()=> {
+        const db = dbService
+        await db.collection("user").add({ 
+            text: write,
+            createdAt: Date.now(),
+            creatorId: userObj.email
+        })
+        setWrite("")
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={write} onChange={onChange} type='text' placeholder='작성해주세요' maxLength={200} />
+                <button onClick={onClick}>작성</button>
+            </form>
+            <div>
+                {writes.map((write)=> (
+                    <WriteButton key={write.id} writeObj={write} isOwner={write.creatorId === userObj.email} />
+                ))}
+            </div>
+        </div>
+    );
 }
 export default Comunity;
