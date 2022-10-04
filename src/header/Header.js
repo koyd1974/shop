29d4b8e748aad2daf86f593cf2dff8391694f53c
koyd9174 <koyd9174@naver.com>
2022-10-04 15:45:51 +0900

    10/4 수정

--- src/header/Header.js
+++ src/header/Header.js
@@ -16,11 +16,11 @@ function Header(props) {
             <h1><NavLink to ='/'style={{paddingLeft: 13, textDecoration: 'none'}}>PetShop</NavLink></h1>
             <ul>
                     <li><NavLink to ='/upload/upload'style={{paddingLeft: 13, textDecoration: 'none'}}>상품등록</NavLink></li>
-                    <li><NavLink to ='/product2/3'style={{paddingLeft: 13, textDecoration: 'none'}}>상품보기</NavLink></li>
-                    <li><NavLink to ='/comunity/4'style={{paddingLeft: 13, textDecoration: 'none'}}>커뮤니티</NavLink></li>
-                    <li><NavLink to ='/cart/5'style={{paddingLeft: 13, textDecoration: 'none'}}>장바구니</NavLink></li>
-                    <li><NavLink to ='/login/6'style={{paddingLeft: 13, textDecoration: 'none', color : 'black'}}><UserOutlined /></NavLink></li>
-                    <li><NavLink to ='/signup/7'style={{paddingLeft: 13, textDecoration: 'none' , color : 'black'}}><UserAddOutlined /></NavLink></li>
+                    <li><NavLink to ='/product2/product2'style={{paddingLeft: 13, textDecoration: 'none'}}>상품보기</NavLink></li>
+                    <li><NavLink to ='/comunity/comunity'style={{paddingLeft: 13, textDecoration: 'none'}}>커뮤니티</NavLink></li>
+                    <li><NavLink to ='/cart/cart'style={{paddingLeft: 13, textDecoration: 'none'}}>장바구니</NavLink></li>
+                    <li><NavLink to ='/login/login'style={{paddingLeft: 13, textDecoration: 'none', color : 'black'}}><UserOutlined /></NavLink></li>
+                    <li><NavLink to ='/signup/signup'style={{paddingLeft: 13, textDecoration: 'none' , color : 'black'}}><UserAddOutlined /></NavLink></li>
                 </ul>
         </Container>
       </Navbar>
