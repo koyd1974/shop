c49ea6f45d5665151795a824a432dfcb694017e3
koyd9174 <koyd9174@naver.com>
2022-10-06 20:24:07 +0900

    cart추가

--- src/main/Cart.js
+++ src/main/Cart.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import {Table} from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux';
+import { dbService } from '../fbase';
+import React, { useEffect, useState } from 'react';
 
-function Cart(props){
-    return(
-        <div>
-    <Table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>상품명</th>
-            <th>가격</th>
-            <th>수량</th>
-            <th>변경</th>
-          </tr>
-        </thead>
-     
-</Table>
-</div>
-    )
+const Cart = ({userObj})=> {
+  const [cart, setCart] = useState([])
+  useEffect(()=> {
+    dbService.collection("Cart").onSnapshot(snapshot=> {
+      const cartArray = snapshot.docs.map((doc)=> ({
+        id: doc.id,
+        ...doc.data()
+      }))
+      const resultArray = cartArray.filter((data)=> {
+        return data.user === userObj.email
+      })
+      setCart(resultArray)
+    })
+  }, [])
+  return(
+    <div>
+      <div>
+        {cart.sort((a,b)=> a.createdAt - b.createdAt).map((data, index)=> (
+          <div key={index}>
+              <ul>
+                <li><img src={data.img} width={50} height={50}/></li>
+                <li>{data.text}원</li>
+              </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Cart;
