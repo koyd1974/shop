97663f85c28e058380749e341c8424394c8bb65b
koyd9174 <koyd9174@naver.com>
2022-10-06 20:37:11 +0900

    login시 login 아이콘 없어짐 기능 추가

--- src/header/Header.js
+++ src/header/Header.js
@@ -38,7 +38,34 @@ const Header = ({isLoggedIn})=> {
     <div id="header">
     <Navbar bg="light" variant="light">
         <Container>
-            <h1><NavLink to ='/'style={{paddingLeft: 13, textDecoration: 'none'}}>PetShop</NavLink></h1>
+        {isLoggedIn ?(
+                        <>
+                            <h1><NavLink to ='/' className='text-link'>PetShop</NavLink></h1>
+                            <ul>
+                                <li><NavLink to ='/upload/2' className='text-link'>상품등록하기</NavLink></li>
+                                <li><NavLink to ='/product2/3' className='text-link'>상품보기</NavLink></li>
+                                <li><NavLink to ='/comunity/4' name='comunity' className='text-link'>커뮤니티</NavLink></li>
+                                <li><NavLink to ='/cart/5' className='text-link'>장바구니</NavLink></li>
+                                {/* <li>{init ? <Router isLoggedIn={isLoggedIn} /> : '!'}</li> */}
+                                <li><NavLink onClick={onClickLogOut} className='text-link'>logout</NavLink></li>
+                            </ul>
+                        </>
+                    ) : (
+                        <>
+                            <h1><NavLink to ='/' className='text-link'>PetShop</NavLink></h1>
+                            <ul>
+                                <li><NavLink to ='/product2/3' className='text-link'>상품보기</NavLink></li>
+                                <li><NavLink to ='/comunity/4' onClick={onClickFunc} className='text-link'>커뮤니티</NavLink></li>
+                                <li><NavLink to ='/cart/5' onClick={onClickFunc} className='text-link'>장바구니</NavLink></li>
+                                {/* <li>{init ? <Router isLoggedIn={isLoggedIn} /> : '!'}</li> */}
+                                <li><NavLink to="/login" className='text-link'>login</NavLink></li>
+                                <li><NavLink to="/signup" className='text-link' >회원가입</NavLink></li>
+                            </ul>
+                        </>
+                    )}
+        
+
+            {/* <h1><NavLink to ='/'style={{paddingLeft: 13, textDecoration: 'none'}}>PetShop</NavLink></h1>
             <ul>
                     <li><NavLink to ='/upload/upload'style={{paddingLeft: 13, textDecoration: 'none'}}>상품등록</NavLink></li>
                     <li><NavLink to ='/product2/product2'style={{paddingLeft: 13, textDecoration: 'none'}}>상품보기</NavLink></li>
@@ -46,7 +73,7 @@ const Header = ({isLoggedIn})=> {
                     <li><NavLink to ='/cart/cart'style={{paddingLeft: 13, textDecoration: 'none'}}>장바구니</NavLink></li>
                     <li><NavLink to ='/login/login'style={{paddingLeft: 13, textDecoration: 'none', color : 'black'}}><UserOutlined /></NavLink></li>
                     <li><NavLink to ='/signup/signup'style={{paddingLeft: 13, textDecoration: 'none' , color : 'black'}}><UserAddOutlined /></NavLink></li>
-                </ul>
+                </ul> */}
         </Container>
       </Navbar>
         
